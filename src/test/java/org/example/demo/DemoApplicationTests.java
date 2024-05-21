423c25a2ab31ea1ffcf81fc875d876588d550feb
Maryna Yeretska <marinka11071979@gmail.com>
2024-05-21 17:25:11 +0300

    Add a simple test method to DemoApplicationTests

--- src/test/java/org/example/demo/DemoApplicationTests.java
+++ src/test/java/org/example/demo/DemoApplicationTests.java
@@ -3,11 +3,17 @@ package org.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.hibernate.validator.internal.util.Contracts.assertTrue;
+
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void simpleTest() {
+		assertTrue(true, "This test will always pass.");
 	}
 
 }
